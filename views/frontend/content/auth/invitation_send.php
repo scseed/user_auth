4fb1df10fbb7231f63bd4638bdb117c7bf7defbf
SMGladkovskiy <sergey.gladkovskiy@studia-art.com>
2012-05-16 11:43:22 +0400

    send invitation template

---
+++ views/frontend/content/auth/invitation_send.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct access allowed.'); ?>
+<div class="row-fluid" style="margin: 5em auto">
+	<div class="span4">&nbsp;</div>
+	<div class="span4">
+		<h3><?php echo __('Отлично!')?></h3>
+		<div class="alert alert-success">
+			Приглашение пользователю отправлено!
+		</div>
+		<?php echo HTML::anchor('/', 'Вернуться')?>
+	</div>
+</div>
